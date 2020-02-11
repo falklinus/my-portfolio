10bd44a186b0ac581769f9fc66c90bbbb1898575
Linus Falk <linfa084@student.liu.se>
2020-02-12 00:11:35 +0100

    some new navigation

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ const navSlide = () => {
   hamburger.addEventListener('click', e => {
     nav.classList.toggle('nav-active');
 
-    /*navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = "";
-        } else {
-          link.style.animation = `navLinkFade 0.3s ease forwards ${index / 10 +
-            0.3}s`;
-        }
-      }); */
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 20 +
+          0.4}s`;
+      }
+    });
 
     hamburger.classList.toggle('toggle');
   });
@@ -32,3 +32,39 @@ const navSlide = () => {
 };
 
 navSlide();
+
+let sidenavItems = document.querySelectorAll('.sidenav li');
+
+sidenavItems.forEach(li => {
+  li.addEventListener('click', () => {
+    clearIcons();
+    let icon = li.querySelector('i');
+    icon.classList.add('fas');
+    let scrollTarget = li.getAttribute('name');
+    if (scrollTarget === 'start') {
+      window.scrollTo({ top: 0 });
+    } else {
+      let scrollElmnt = document.querySelector(`#${scrollTarget}`);
+      scrollElmnt.scrollIntoView();
+    }
+  });
+});
+
+function clearIcons() {
+  sidenavItems.forEach(li => {
+    let icon = li.querySelector('i');
+    icon.classList.remove('fas');
+    icon.classList.add('far');
+  });
+}
+
+/* let cursor = document.querySelector('#cursor');
+
+window.addEventListener('mousemove', moveCursor);
+
+function moveCursor(e) {
+  console.log(cursor);
+  cursor.style.top = `${e.pageY}px`;
+  cursor.style.left = `${e.pageX}px`;
+  console.log(e);
+} */
